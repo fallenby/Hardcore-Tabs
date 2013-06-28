24706bd4286fa88ec4ece374ef4e2e708ec4d5f9
Ye7i <teh.ye7i@gmail.com>
2013-06-28 23:45:10 +0200

    onEnable onDisable, yaml

--- hardcore-tabs/src/io/github/ye7i/hardcore_tabs/HardcoreTabs.java
+++ hardcore-tabs/src/io/github/ye7i/hardcore_tabs/HardcoreTabs.java
@@ -2,11 +2,23 @@ package io.github.ye7i.hardcore_tabs;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class HardcoreTabs extends JavaPlugin {
+public final class HardcoreTabs extends JavaPlugin {
 
 	public HardcoreTabs()
 	{
-		// Hello world!
+
+	}
+	
+	@Override
+	public void onEnable()
+	{
+		getLogger().info("Fuck");
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		
 	}
 	
 }
