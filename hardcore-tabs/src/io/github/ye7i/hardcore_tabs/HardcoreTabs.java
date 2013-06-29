89f5b473243fdef96e0be674f95f0daea71c5e85
Ye7i <teh.ye7i@gmail.com>
2013-06-29 22:52:45 +0200

    Plugin enters alpha

--- hardcore-tabs/src/io/github/ye7i/hardcore_tabs/HardcoreTabs.java
+++ hardcore-tabs/src/io/github/ye7i/hardcore_tabs/HardcoreTabs.java
@@ -1,32 +1,28 @@
 package io.github.ye7i.hardcore_tabs;
 
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HardcoreTabs extends JavaPlugin {
-
+	
+	public ConfigAccessor groupConfig = null;
+	
 	@Override
 	public void onEnable()
 	{
-		getLogger().info(getConfig().getString("PLUGIN_NAME") + " " + getConfig().getString("MSG_ENABLE"));
-	
-		Player[] onlinePlayers = getServer().getOnlinePlayers();
-		for (Player player : onlinePlayers)
-		{
-			//getLogger().info("Set player " + player.getDisplayName() + " name to " + "§4" + player.getDisplayName());
-			player.setDisplayName(player.getName());
-			if (player.isOp())
-				player.setPlayerListName("§2§l§o" + player.getName());
-		}
+		groupConfig = new ConfigAccessor(this, getConfig().getString("GROUP_CONFIG_FILE_NAME"));
+		groupConfig.saveDefaultConfig();
 		
-		getCommand("bcast").setExecutor(new CommandExecutorBroadcast(this));
-		getCommand("test").setExecutor(new CommandExecutorTest(this));
+		getCommand("hctreload").setExecutor(new CommandExecutorReload(this));
+		
+		getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
+		
+		getLogger().info(String.format("$0 $1 $2", getDescription().getName(), getDescription().getVersion(), getConfig().getString("MSG_ENABLE")));
 	}
 	
 	@Override
 	public void onDisable()
 	{
-		getLogger().info(getConfig().getString("PLUGIN_NAME") + " " + getConfig().getString("MSG_DISABLE"));
+		getLogger().info(String.format("$0 $1 $2", getDescription().getName(), getDescription().getVersion(), getConfig().getString("MSG_DISABLE")));
 	}
 	
 }
