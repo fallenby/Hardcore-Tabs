9e9e8fa733694805b9c7f2ed052e8701793b357f
Ye7i <teh.ye7i@gmail.com>
2013-06-29 16:36:47 +0200

    experimental color/font changes

--- hardcore-tabs/src/io/github/ye7i/hardcore_tabs/HardcoreTabs.java
+++ hardcore-tabs/src/io/github/ye7i/hardcore_tabs/HardcoreTabs.java
@@ -1,24 +1,32 @@
 package io.github.ye7i.hardcore_tabs;
 
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HardcoreTabs extends JavaPlugin {
 
-	public HardcoreTabs()
-	{
-
-	}
-	
 	@Override
 	public void onEnable()
 	{
-		getLogger().info("Fuck");
+		getLogger().info(getConfig().getString("PLUGIN_NAME") + " " + getConfig().getString("MSG_ENABLE"));
+	
+		Player[] onlinePlayers = getServer().getOnlinePlayers();
+		for (Player player : onlinePlayers)
+		{
+			//getLogger().info("Set player " + player.getDisplayName() + " name to " + "§4" + player.getDisplayName());
+			player.setDisplayName(player.getName());
+			if (player.isOp())
+				player.setPlayerListName("§2§l§o" + player.getName());
+		}
+		
+		getCommand("bcast").setExecutor(new CommandExecutorBroadcast(this));
+		getCommand("test").setExecutor(new CommandExecutorTest(this));
 	}
 	
 	@Override
 	public void onDisable()
 	{
-		
+		getLogger().info(getConfig().getString("PLUGIN_NAME") + " " + getConfig().getString("MSG_DISABLE"));
 	}
 	
 }
